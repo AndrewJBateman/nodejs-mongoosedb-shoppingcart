eff7b235b3edfc0742aac431ab7a597c6699fab4
Andrew Bateman <gomezbateman@yahoo.com>
2019-01-14 21:42:18 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Node.js, mongoDB & Mongoose Shopping Cart
+
+Code to learn to learn Node.js. This is part of a Udemy Node.js course.
+Working shopping cart where products can be created, read, updated and deleted (CRUD) via localhost:3000 and are stored in a mongoDB database. Products can be added to a cart and 'ordered' under an auto-generated order number. 
+
+This project uses [MongoDB Atlas](https://www.mongodb.com/cloud/atlas) version 4.0 and [MongoDB Compass](https://www.mongodb.com/download-center/compass) version 1.16.3 on an Azure cloud server. 
+
+[Mongoose](https://mongoosejs.com) version 5.2.17 is used to model the application data. 
+
+This project also uses [Express](http://expressjs.com/) version 4.16.4.
+
+
+## Notes
+Link to Udemy Course [NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)](https://www.udemy.com/nodejs-the-complete-guide/).<br>
---
+++ app.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5c3c5fb4cda82c40382e0f8a')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    'mongodb+srv://user1:2vE6ll3YrUEJm9y3@cluster0-kaj5u.azure.mongodb.net/shop', { useNewUrlParser: true }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Andrew',
+          email: 'Andrew@gmail.com',
+          cart: {
+            items: []        
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
